2094065dec57923dd1dc73308541ad138c8fa97a
ogulcansahna <61863684+ogulcansahna@users.noreply.github.com>
2020-11-23 06:51:34 +0300

    hw

--- app/src/main/java/ise308/sahnaoglu/ogulcan/constructors/Main.kt
+++ app/src/main/java/ise308/sahnaoglu/ogulcan/constructors/Main.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>){
     val araba4 = Araba("bmw","540",1999,160000)
     val araba5 = Araba("bmw","320",2017,450000)
     val anahtar = 1500
- //   val araba7 = indirim + anahtar
+ // val araba7 = indirim + anahtar
     //overload
     araba1.fiyat = araba1.fiyat.plus(anahtar)
     araba2.fiyat = araba2.fiyat.plus(anahtar)
@@ -22,15 +22,17 @@ fun main(args: Array<String>){
     println("Arac Adı: ${araba4.marka} ${araba4.ismi} Modeli: ${araba4.yas} Fiyatı: ${araba4.fiyat} ")
     println("Arac Adı: ${araba5.marka} ${araba5.ismi} Modeli: ${araba5.yas} Fiyatı: ${araba5.fiyat} ")
 }
+
+//privateclass,settergetter
 private class Musteri (var Ad : String, odeme : Int){
     private var Musteri1 = Musteri("Mehmet",30000)
-    set(value) {
-        Musteri1}
+        get() = field
+        set(value) {field = Musteri1}
 
     private var Musteri2 = Musteri("ali",250000)
 }
 
-//parent
+//parent & constructor
 open class Arac (var vasita: String){
     init {
         println("Elimizde $vasita modelinde araçlar mevcuttur.")
